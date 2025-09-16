34ffc3070dd2f8dad8ac072625b972b1d78589f7
Chheangleanghai <cheangleanghai60@gmail.com>
2025-09-16 20:12:07 +0700

    Lastupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {Routes, Route, useLocation } from "react-router-dom";
+import {HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MobileNav from "./components/MobileNav";
 import Footer from "./components/Footer";
